934d24f1b1fd1fdf4ee6728c64c89dd08ee723e1
Vestri <p.prokop@firenja.com>
2020-06-16 12:15:15 +0200

    fixes add all worldguard flags change expre text : "get %string% of %region%" string = flag value

--- src/main/java/elements/expressions/ExprWorldGuardFlag.java
+++ src/main/java/elements/expressions/ExprWorldGuardFlag.java
@@ -1,35 +1,31 @@
 package main.java.elements.expressions;
 
 import ch.njol.skript.Skript;
-import ch.njol.skript.SkriptCommand;
-import ch.njol.skript.command.CommandEvent;
-import ch.njol.skript.command.ScriptCommandEvent;
-import ch.njol.skript.hooks.regions.RegionsPlugin;
 import ch.njol.skript.hooks.regions.classes.Region;
-import ch.njol.skript.hooks.regions.events.RegionBorderEvent;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
-import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.flags.*;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.event.Event;
 
 import javax.annotation.Nullable;
 
+@SuppressWarnings("unused")
 public class ExprWorldGuardFlag extends SimpleExpression<String>
 {
-
-    private static Expression<Region> region;
+    @SuppressWarnings("null")
+    private Expression<Region> region;
+    private String flag;
 
     static {
-        Skript.registerExpression(ExprWorldGuardFlag.class, String.class, ExpressionType.COMBINED, "[the] pvpflag of %region%");
+        Skript.registerExpression(ExprWorldGuardFlag.class, String.class, ExpressionType.SIMPLE,"get %string% of %region%");
     }
 
     @Override
@@ -42,10 +38,11 @@ public class ExprWorldGuardFlag extends SimpleExpression<String>
         return true;
     }
 
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings({"unchecked", "null"})
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parser) {
-        region = (Expression<Region>) exprs[0];
+        flag = exprs[0].toString().replace("\"","");
+        region = (Expression<Region>) exprs[1];
         return true;
     }
 
@@ -61,11 +58,39 @@ public class ExprWorldGuardFlag extends SimpleExpression<String>
         return new String[] {canPvPHere(event)};
     }
 
-    public static String canPvPHere(Event eve)
+    @SuppressWarnings({"unchecked", "null"})
+    public String canPvPHere(Event eve)
     {
-        Region reg = region.getSingle(eve);
-        RegionContainer container = com.sk89q.worldguard.WorldGuard.getInstance().getPlatform().getRegionContainer();
-        RegionManager regions = container.get(BukkitAdapter.adapt( Bukkit.getServer().getWorld(reg.toString().split(" ")[reg.toString().split(" ").length-1])));
-        return regions.getRegion(reg.toString().split(" in ")[0]).getFlag(Flags.PVP).toString();
+        try
+        {
+            Region reg = region.getSingle(eve);
+            RegionContainer container = com.sk89q.worldguard.WorldGuard.getInstance().getPlatform().getRegionContainer();
+            RegionManager regions = container.get(BukkitAdapter.adapt( Bukkit.getServer().getWorld(reg.toString().split(" ")[reg.toString().split(" ").length-1])));
+
+            if (StateFlag.isValidName(flag))
+            {
+                try
+                {
+                    return regions.getRegion(reg.toString().split(" in ")[0]).getFlag(getFlagValue(flag)).toString();
+                }
+                catch(Exception ex)
+                {
+                    return "flag not found";
+                }
+
+            }
+            return "flag not found";
+        }
+        catch (Exception ex)
+        {
+            return "you did make something wrong";
+        }
+
+    }
+
+    private Flag getFlagValue(String flagname)
+    {
+        Flag<?> flag = WorldGuard.getInstance().getFlagRegistry().get(flagname);
+        return flag;
     }
 }
